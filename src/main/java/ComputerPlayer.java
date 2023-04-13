dc992ee28fda84d63c720dcb04ec86a1cb6fa10e
Maksym Zinchenko <maklaut98@gmail.com>
2023-04-12 23:16:13 -0500

    Can play against computer

--- src/main/java/ComputerPlayer.java
+++ src/main/java/ComputerPlayer.java
@@ -1,7 +1,17 @@
+import java.util.Random;
 public class ComputerPlayer extends Player{
 
     @Override
     public void nextTurn(){
-        System.out.println("PC");
+        Random rand = new Random();
+        int randNum = rand.nextInt(3);
+        System.out.println(randNum);
+        if(randNum == 0 ){
+            this.setSign("paper");
+        } else if(randNum == 1 ){
+            this.setSign("rock");
+        } else {
+            this.setSign("scissors");
+        }
     }
 }
